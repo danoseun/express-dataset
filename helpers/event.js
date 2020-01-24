74fdafc732f275bb8630cca20ddcc28fbc39d773
danoseun <ayodejisomefun@yahoo.com>
2020-01-24 11:24:42 +0100

    Add new files and implement the stated features to pass tests cases

--- helpers/event.js
+++ helpers/event.js
@@ -1,1 +1,8 @@
 const calculateTime = date => new Date(date).getTime();
+
+module.exports = {
+    orderInAscendingOrderById: data => {
+      let result = data.sort((a,b) => Number(a.id) - Number(b.id));
+      return result;
+    }
+}
--- routes/actor.js
+++ routes/actor.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const ActorController = require('../controllers/actors');
 
 // Routes related to actor.
+router.put('/', ActorController.updateActor);
+router.get('/', ActorController.getAllActors);
+router.get('/streak', ActorController.getStreak);
 
 module.exports = router;
